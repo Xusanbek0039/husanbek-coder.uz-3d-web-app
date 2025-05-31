372a658df59d2d81ee0bf77e69da0b88ce957e9b
Xusanbek Suyunov <109731657+Xusanbek0039@users.noreply.github.com>
2025-05-31 20:23:00 +0500

    Update page.tsx

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function Page() {
   return (
     <div className="container mx-auto text-zinc-300 flex justify-center items-center h-full">
-      <h1 className="text-3xl">Hech qanday blog postlari yo&apos;q!</h1>
+      <h1 className="text-3xl">Hech qanday blog postlari yo&#39;q!</h1>
     </div>
   );
 }
